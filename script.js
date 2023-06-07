16a7cfbbbb6135e895e7c77b948f96a9ac04ae6c
imran <imran@yandex.ru>
2023-06-07 23:14:11 +0300

    add two files

---
+++ script.js
@@ -0,0 +1,17 @@
+//Homework num.1 with Js
+let j=20;
+j=40;
+console.log (j)
+let a=2;
+let b=2;
+let c=2;
+let d=a+b*c;
+console.log (d)//wtf?)))
+let person={
+    name: "Imran",
+    age: 35,
+    isStudent: true
+}
+console.log (person)
+let message="Goodbye World";
+console.log (message)
