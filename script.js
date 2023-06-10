7cb9d01d7c6ae8e4f53c92743e59791c5a0ccf9c
imran <imran@yandex.ru>
2023-06-10 18:38:14 +0300

    finished homework num.1

--- script.js
+++ script.js
@@ -7,11 +7,21 @@ let b=2;
 let c=2;
 let d=a+b*c;
 console.log (d)//wtf?)))
-let person={
-    name: "Imran",
-    age: 35,
-    isStudent: true
-}
-console.log (person)
+
 let message="Goodbye World";
 console.log (message)
+
+var name;
+var address;
+var phone;
+    if(confirm("Do you want to register?")) {
+         name = prompt("What is your name?");
+         alert("Hi, " + name)
+         address = prompt("Where are you from?" + name);
+         phone = prompt("enter your number!" + name);
+    } else
+    alert ("go away!!!:(");
+
+console.log ("Студент - " + name)
+console.log ("Адрес - " + address)
+console.log ("Номер телефона - " + phone)
