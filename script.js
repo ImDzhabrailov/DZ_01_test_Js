b615cf27fba8a18c1654dca47cd959516ef3fffc
imran <imran@yandex.ru>
2024-08-21 16:44:00 +0300

    first

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//Homework num.1 with Js
 let j=20;
 j=40;
 console.log (j)
@@ -6,7 +5,7 @@ let a=2;
 let b=2;
 let c=2;
 let d=a+b*c;
-console.log (d)//wtf?)))
+console.log (d)//
 
 let message="Goodbye World";
 console.log (message)
